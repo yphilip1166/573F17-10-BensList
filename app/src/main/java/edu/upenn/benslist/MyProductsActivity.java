b28515febbf7b8354e752582ddb2be0a6b627943
Tyler Douglas <tylerd@seas.upenn.edu>
2017-03-27 17:37:01 -0400

    added to user class, search function, profile picture

---
+++ app/src/main/java/edu/upenn/benslist/MyProductsActivity.java
@@ -0,0 +1,8 @@
+package edu.upenn.benslist;
+
+/**
+ * Created by tylerdouglas on 3/27/17.
+ */
+
+public class MyProductsActivity {
+}
---
+++ app/src/main/java/edu/upenn/benslist/UserProfile.java
@@ -0,0 +1,10 @@
+package edu.upenn.benslist;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tylerdouglas on 3/26/17.
+ */
+
+public class UserProfile extends AppCompatActivity {
+}
