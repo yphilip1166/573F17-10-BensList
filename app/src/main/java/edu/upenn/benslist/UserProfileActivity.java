02249397691732615fc76acd385fbcd3d23efa0b
Shawn Jiang <shawnjiang1995@gmail.com>
2017-11-30 21:56:38 -0500

    fixed view previous purchased items

--- app/src/main/java/edu/upenn/benslist/UserProfileActivity.java
+++ app/src/main/java/edu/upenn/benslist/UserProfileActivity.java
@@ -139,16 +139,16 @@ public class UserProfileActivity extends AppCompatActivity {
 //            }
 //        });
 
-//        Button previousPurchases = (Button) findViewById(R.id.prevousProducts);
-//        previousPurchases.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i = new Intent(v.getContext(), ViewUploadedProductsActivity.class);
-//                i.putExtra("Type", "previousPurchases");
-//                i.putExtra("UserId", currentUserID);
-//                startActivity(i);
-//            }
-//        });
+        Button previousPurchases = (Button) findViewById(R.id.prevousProducts);
+        previousPurchases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), ViewUploadedProductsActivity.class);
+                i.putExtra("Type", "previousPurchases");
+                i.putExtra("UserId", currentUserID);
+                startActivity(i);
+            }
+        });
 
 
         Button favoriteUsers = (Button) findViewById(R.id.favoriteUsers);
